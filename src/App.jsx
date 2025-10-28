edc921fc1aa9d64ef9238d427951f04efc3c371a
vinitJenya <vinitshah.jenya@gmail.com>
2025-10-28 22:38:04 +0530

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
 import { Button } from "./Components/Button";
 
 function App() {
-  return <>{/* <Button variant="success">Test Button</Button> */}</>;
+  return (
+    <>
+      <Button variant="success">Test Button</Button>
+    </>
+  );
 }
 
 export default App;
